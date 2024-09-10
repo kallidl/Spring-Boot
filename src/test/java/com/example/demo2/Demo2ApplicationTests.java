4f3db7f02e27bbbb1459c899b39b503f619f1e2f
kallidl <3046507560@qq.com>
2024-09-10 16:03:34 +0800

    Environment对象

--- src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.demo2.domain.Consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class Demo2ApplicationTests {
@@ -11,10 +12,20 @@ class Demo2ApplicationTests {
     @Autowired
     private Consumer consumer;
 
+    @Autowired
+    private Environment env;
 
     @Test
     void wiredTest() {
         System.out.println(consumer);
     }
 
+
+    @Test
+    void evnTest() {
+        System.out.println("consumer.username=" + env.getProperty("consumer.username"));
+        System.out.println("consumer.age=" + env.getProperty("consumer.age"));
+        System.out.println("consumer.hobby=" + env.getProperty("consumer.hobby"));
+        System.out.println("consumer.subject=" + env.getProperty("consumer.subject"));
+    }
 }
